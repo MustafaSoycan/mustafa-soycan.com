268b0146f23e6f54465d47163b1785e7f691334b
MustafaSoycan <mustafa.soycan@outlook.com>
2023-06-10 03:03:57 +0200

    few design changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  ngOnInit() {
+    const menu_btn = document.querySelector('.hamburger') as HTMLElement;
+    const mobile_menu = document.querySelector('.mobile-nav') as HTMLElement;
+    menu_btn.addEventListener('click', function(){
+      menu_btn.classList.toggle('is-active');
+      mobile_menu.classList.toggle('is-active');
+    });
+  }
 }

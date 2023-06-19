cb6ca6081eebe074e50709258817360df3487618
MustafaSoycan <mustafa.soycan@outlook.com>
2023-06-19 13:04:41 +0200

    few design fixes

--- src/app/contact-area/contact-area.component.ts
+++ src/app/contact-area/contact-area.component.ts
@@ -6,39 +6,61 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./contact-area.component.scss']
 })
 export class ContactAreaComponent {
-
   @ViewChild('myForm') myForm!: ElementRef;
   @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('emailField') emailField!: ElementRef;
   @ViewChild('messageField') messageField!: ElementRef;
   @ViewChild('sendButton') sendButton!: ElementRef;
 
-  sendMail(){
-    // action="https://mustafa-soycan.com//mustafa-soycan.com/send_mail/send_mail.php" 
+  sendingMail: boolean = false;
+  sendButtonText: string = 'Send Message :)';
 
+  sendMail() {
     console.log('sending mail', this.myForm);
     let nameField = this.nameField.nativeElement;
     let messageField = this.messageField.nativeElement;
+    let emailField = this.emailField.nativeElement;
     let sendButton = this.sendButton.nativeElement;
-   
+
     nameField.disabled = true;
+    emailField.disabled = true;
     messageField.disabled = true;
     sendButton.disabled = true;
-    // Animation anzeigen
+
     let fd = new FormData()
     fd.append('name', nameField.value);
+    fd.append('email', emailField.value);
     fd.append('message', messageField.value);
-    // senden
-    fetch("https://mustafa-soycan.com//mustafa-soycan.com/send_mail/send_mail.php",
-    {
+
+    this.sendingMail = true;
+    this.sendButtonText = 'Sending...';
+
+    fetch("https://mustafa-soycan.com/send_mail/send_mail.php", {
       method: 'POST',
       body: fd
-    }
-    );
+    })
+      .then(response => {
+        if (response.ok) {
+          this.sendButtonText = 'Submitted successfully :)';
+          this.myForm.nativeElement.reset();
+        } else {
+          this.sendButtonText = 'Failed to send email';
+        }
+        this.sendingMail = false;
 
-    // Text anzeigen: Nachricht gesendet!
-    nameField.disabled = false;
-    messageField.disabled = false;
-    sendButton.disabled = false;
+        nameField.disabled = false;
+        emailField.disabled = false;
+        messageField.disabled = false;
+        sendButton.disabled = false;
+      })
+      .catch(error => {
+        this.sendButtonText = 'Failed to send email :(';
+        this.sendingMail = false;
 
+        nameField.disabled = false;
+        emailField.disabled = false;
+        messageField.disabled = false;
+        sendButton.disabled = false;
+      });
   }
 }
--- src/app/skills-area/skills-area.component.ts
+++ src/app/skills-area/skills-area.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-skills-area',
   templateUrl: './skills-area.component.html',
   styleUrls: ['./skills-area.component.scss']
 })
-export class SkillsAreaComponent {
-
+export class SkillsAreaComponent{
+ 
 }

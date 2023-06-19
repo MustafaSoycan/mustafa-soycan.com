f1f101544ea8041b872b229f7940127c0f7a6f86
MustafaSoycan <mustafa.soycan@outlook.com>
2023-06-19 13:14:23 +0200

    burger menu update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,9 +10,24 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
     const menu_btn = document.querySelector('.hamburger') as HTMLElement;
     const mobile_menu = document.querySelector('.mobile-nav') as HTMLElement;
-    menu_btn.addEventListener('click', function(){
+  
+    const links = mobile_menu.querySelectorAll('a'); // Alle Links im Burger-Menü auswählen
+  
+    // Funktion zum Schließen des Burger-Menüs
+    const closeMenu = function() {
+      menu_btn.classList.remove('is-active');
+      mobile_menu.classList.remove('is-active');
+    };
+  
+    // Event Listener für den Klick auf den Hamburger-Button
+    menu_btn.addEventListener('click', function() {
       menu_btn.classList.toggle('is-active');
       mobile_menu.classList.toggle('is-active');
+    });
+  
+    // Event Listener für den Klick auf einen Link im Burger-Menü
+    links.forEach(function(link) {
+      link.addEventListener('click', closeMenu);
     });
   }
 }
